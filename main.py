abf64b8a5c8039034801dee4a1704c6debc0ae8d
JessiP23 <jessi316866@gmail.com>
2024-12-19 17:32:13 -0500

    user

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #import relevant functionality
+from langchain_anthropic import ChatAnthropic
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import HumanMessage
+from langchain.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import create_react_agent
+
+
+# Create the agent
+memory = MemorySaver()
+model = ChatAnthropic(model_name="claude-3-sonnet-20240229")
+search = TavilySearchResults(max_results=3)
+tools = [search]
+agent_executor = create_react_agent(model, tools, checkpointer=memory)
+
+
+# use the agent
