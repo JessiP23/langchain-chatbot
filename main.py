e87f297ab22ee043f162476d54631906a33d32c9
JessiP23 <jessi316866@gmail.com>
2024-12-19 21:51:39 -0500

    ate

--- main.py
+++ main.py
@@ -4,12 +4,22 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.messages import HumanMessage
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
+import os
+
+os.environ["TAVILY_API_KEY"] = "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"
 
 
 # Create the agent
 memory = MemorySaver()
 model = ChatAnthropic(model_name="claude-3-sonnet-20240229")
-search = TavilySearchResults(max_results=3)
+search = TavilySearchResults(
+    max_results=3,
+    include_answer=True,
+    include_raw_content=True,
+    include_images=True,
+)
+
+print("Tavily was instatiated")
 tools = [search]
 agent_executor = create_react_agent(model, tools, checkpointer=memory)
 
